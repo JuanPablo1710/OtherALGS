d90067bc6cfb3bdf80d57d1f247a5fcefcef945c
JuanPablo1710 <70925749+JuanPablo1710@users.noreply.github.com>
2023-04-01 11:00:51 -0600

    Create Heap_staticArray.cpp

---
+++ Heap_staticArray.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct THeap{
+	int data[10000];
+	int index;
+
+	THeap(){
+		index = 0;
+	}
+
+	int size(){ return index; }
+
+	bool empty(){ return index == 0; }
+
+	void print(){
+		for(int i = 0; i < index; i++) cout << data[i] << " ";
+	}
+
+	int parent(int i){ return (i -1) / 2; }
+
+	void push(int x){
+
+		int tempinx = index, pindex = parent(tempinx);
+		data[index] = x; 
+
+		while(tempinx > 0 && data[tempinx] > data[pindex]){
+
+			swap(data[tempinx], data[pindex]);
+
+			tempinx = pindex;
+			pindex = parent(tempinx);
+		}
+
+		index++;
+	}
+
+};
+
+int main() {
+	
+	int n; 
+	cin >> n;
+
+	THeap tree;
+
+	for(int i = 0; i < n; i++){
+		int x;
+		cin >> x;
+		tree.push(x);
+	}
+
+	tree.print();
+
+	return 0;
+}
