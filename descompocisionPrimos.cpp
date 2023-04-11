049de99b9f78a60537ac7cf330fc6428371cc353
JuanPablo1710 <70925749+JuanPablo1710@users.noreply.github.com>
2023-04-11 12:30:07 -0600

    Create descompocisionPrimos.cpp
    Algoritmo que imprime la descomposición en primos de un numero dado

---
+++ descompocisionPrimos.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool es_primo(int n){
+	int sq = sqrt(n);
+
+	for(int i = 2; i <= sq; i++) if(n % i == 0){
+		return false;
+	}
+
+	return true;
+}
+
+int sig_primo(int n){
+	n++;
+	while(true)
+		if(es_primo(n)) return n;
+		else n++;
+}
+
+int main(){
+	
+	int n, actpr = 2, exp;
+	cin >> n;
+	
+	while(n != 1){
+
+		exp = 0;
+
+		while(n % actpr == 0){
+			n /= actpr;
+			exp++;
+		}
+
+		if(exp) cout << actpr << " " << exp << '\n';
+
+		actpr = sig_primo(actpr);
+	}
+
+	return 0;
+}
